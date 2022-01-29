13fe56dc925ed97d429f8305d34e0242fdbc328d
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-01-30 00:56:41 +0700

    add component in page and edit seed

--- app/admin_shared/page_admin/index.rb
+++ app/admin_shared/page_admin/index.rb
@@ -6,11 +6,11 @@ module PageAdmin
 				index :as => :sortable do
 					label :title # item content
 					actions defaults: false do |page|
-						(link_to add_children_admin_page_path(page), style: "margin: 0 0.5rem;" do icon('fas', 'plus') end) +
-						(link_to edit_js_admin_page_path(page), remote: :true, style: "margin: 0 0.5rem;" do icon('fas', 'edit') end) +
+						(link_to add_children_admin_page_path(page), class: "mx-2" do icon('fas', 'plus') end) +
+						(link_to edit_js_admin_page_path(page), remote: :true, class: "mx-2 text-success" do icon('fas', 'edit') end) +
 
 						# (link_to edit_admin_page_path(page), style: "margin: 0 0.5rem;" do icon('fas', 'edit') end) +
-						(link_to admin_page_path(page), style: "margin: 0 0.5rem;", :method => :delete, :data => {:confirm => "Are you sure?"} do icon('fas', 'trash') end)
+						(link_to admin_page_path(page), class: "mx-2 text-danger", :method => :delete, :data => {:confirm => "Are you sure?"} do icon('fas', 'trash') end)
 					end
 
 					render "load_form"
