6efa5d770afcb29aabcf004e4fb0c7d26e1aa249
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-06-11 11:49:16 +0700

    Fixing form component edit

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,5 +1,6 @@
 class Component < ApplicationRecord
 
+  default_scope { order(:position) }
   # attachment
   has_many :attachments,  class_name: "Attachment",  as: "parent", dependent: :destroy
   accepts_nested_attributes_for :attachments, :allow_destroy => true
