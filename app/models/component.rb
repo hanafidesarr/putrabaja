051128c62c889711b52084f1800688599ca74600
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-07-23 00:00:56 +0700

    add text image component

--- app/models/component.rb
+++ app/models/component.rb
@@ -6,5 +6,5 @@ class Component < ApplicationRecord
   accepts_nested_attributes_for :attachments, :allow_destroy => true
   belongs_to :page
 
-  COMPONENT_LAYOUT=["banner","banner_full","custom_categories_products","text_base","text_maqruee_base","text_product_base"]
+  COMPONENT_LAYOUT=["banner","banner_full","custom_categories_products","text_base","text_image_base","text_maqruee_base","text_product_base"]
 end
