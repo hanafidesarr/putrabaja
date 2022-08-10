2fcf2f704fa3e22bfa04b82115ae2c02c9071710
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-08-10 23:02:56 +0700

    change spinner and nav header

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -9,8 +9,8 @@ ActiveAdmin.register Page do
   before_filter :skip_sidebar!, :only => :index
   
   permit_params :title, :active, :description, :layout, :_destroy,
-  components_attributes: [:id, :name, :layout, :description, :position, :url_product, :url_text, :_destroy, attachments_attributes: [:id, :asset, :content_type, :note, :title, :url_product, :url, :_destroy]], # *note add id for disable duplicate when attribute updated
-  attachments_attributes: [:id, :asset, :content_type, :note, :title, :_destroy] # *note add id for disable duplicate when attribute updated 
+  components_attributes: [:id, :name, :layout, :description, :position, :url_product, :url_text, :_destroy, attachments_attributes: [:id, :active, :asset, :content_type, :note, :title, :url_product, :url, :_destroy]], # *note add id for disable duplicate when attribute updated
+  attachments_attributes: [:id, :active, :asset, :content_type, :note, :title, :_destroy] # *note add id for disable duplicate when attribute updated 
   
   sortable tree: true,
             max_levels: 0,               # infinite indent levels
