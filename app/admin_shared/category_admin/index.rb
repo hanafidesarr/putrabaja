1a2dd30d499efb7d8ce2cbd1c17364c27df0d05d
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-08-16 22:42:29 +0700

    Add sortable in category admin

--- app/admin_shared/category_admin/index.rb
+++ app/admin_shared/category_admin/index.rb
@@ -2,13 +2,20 @@ module CategoryAdmin
 	module Index
 		def self.included(base)
 			base.instance_eval do
+        index :as => :sortable do
+          label :active # item content
+          label :name # item content
+          actions
+        end
         index do
           selectable_column
           id_column
           column :name
+          column :active
           column do |category|
             category.is_root?
           end
+      
           actions
         end
 			end
