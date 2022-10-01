d2afa763e4bd991d667f23e2dba6d238bc4895d7
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-10-01 21:51:57 +0700

    redesign header and banner

--- app/admin_shared/setting_admin/form.rb
+++ app/admin_shared/setting_admin/form.rb
@@ -6,39 +6,42 @@ module SettingAdmin
           div class: "setting-form" do
             tabs do
               tab 'General', style: "list-style-type: none;" do
-                columns do
-                  [:name, :email, :phone_1, :phone_2].each do |obj|
-                    column do
-                      f.input obj, input_html: {class: "w-100 form-control"}
+                panel "Global Setting" do
+                  columns do
+                    [:name, :email, :phone_1, :phone_2].each do |obj|
+                      column do
+                        f.input obj, input_html: {class: "w-100 form-control"}
+                      end
                     end
                   end
-                end
-                columns do
-                  [:address, :location].each do |obj|
-                    
-                    column do
-                      f.input obj, input_html: {class: "w-100 form-control"}
+                  columns do
+                    [:address, :location].each do |obj|
+                      column do
+                        f.input obj, input_html: {class: "w-100 form-control"}
+                      end
                     end
                   end
-                end
-                columns do
-                  column do
-                    f.input :short_description, input_html: {class: "w-100 form-control"}
+                  columns do
+                    column do
+                      f.input :short_description, input_html: {class: "w-100 form-control"}
+                    end
                   end
-                end
-                columns do
-                  column do
-                    f.input :description, input_html: {rows: 5, class: "w-100 form-control"}
+                  columns do
+                    column do
+                      f.input :description, input_html: {rows: 5, class: "w-100 form-control"}
+                    end
                   end
                 end
               end
-              tab 'General style', style: "list-style-type: none;" do
-                yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
 
-                columns do
-                  yml_setting[:general_style].each do |obj|
-                    column do
-                      f.input obj, input_html: {class: "w-100 form-control"}
+              tab 'Style', style: "list-style-type: none;" do
+                yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
+                panel "Body" do
+                  columns do
+                    yml_setting[:general_style].each do |obj|
+                      column do
+                        f.input obj, input_html: {class: "w-100 form-control"}
+                      end
                     end
                   end
                 end
@@ -47,7 +50,9 @@ module SettingAdmin
                     yml_setting[:header_style].each do |obj|
                       column do
                         if obj.eql?("layout")
-                          f.input obj, as: :select, collection: ["layout_left", "layout_center", "layout_right"], input_html: {class: "w-100 form-control select2"}
+                          f.input obj, as: :select, collection: ["layout_left", "layout_center", "layout_right"], input_html: {class: "w-100 form-control"}
+                        elsif obj.eql?("position")
+                          f.input obj, as: :select, collection: ["absolute", "relative"], input_html: {class: "w-100 form-control"}
                         else
                           f.input obj, input_html: {class: "w-100 form-control"}
                         end
@@ -56,46 +61,56 @@ module SettingAdmin
                   end
                 end
               end
+
               tab 'Media Social', style: "list-style-type: none;" do
-                columns do
-                  yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
-                  yml_setting[:url_media_socials].each do |obj|
-                    column do
-                      f.input obj, input_html: {class: "w-100 form-control"}
+                panel "Media social" do
+                  columns do
+                    yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
+                    yml_setting[:url_media_socials].each do |obj|
+                      column do
+                        f.input obj, input_html: {class: "w-100 form-control"}
+                      end
                     end
                   end
-                end
-                columns do
-                  [:youtube_url, :map_url].each do |obj|
-                    
-                    column do
-                      f.input obj, input_html: {class: "w-100 form-control"}
+                  columns do
+                    [:youtube_url, :map_url].each do |obj|
+                      
+                      column do
+                        f.input obj, input_html: {class: "w-100 form-control"}
+                      end
                     end
                   end
                 end
               end
+
               tab 'Attachment' do
-                f.has_many :images, allow_destroy: true, panel: false, heading: false, new_record: true, class: "col-2 mx-2" do |attach|
-                  arbre do
-                    attach.input :asset, :as => :file, label: false, :hint => attach.template.image_tag(attach.object.asset.url(:thumb), class: "w-100"), "data-placement"=>"top"
-                  end
-									attach.template.concat(Arbre::Context.new do
-                    hr
-										div class: "row" do
-                      div class: "col-12" do
-                        attach.template.render partial: "admin/admin_shared/image_upload", locals: {f: attach}
-                      end
+                panel "Image" do
+                  f.has_many :images, allow_destroy: true, panel: false, heading: false, new_record: true, class: "col-2 mx-2" do |attach|
+                    arbre do
+                      attach.input :asset, :as => :file, label: false, :hint => attach.template.image_tag(attach.object.asset.url(:thumb), class: "w-100"), "data-placement"=>"top"
                     end
-                  end.to_s)
+                    attach.template.concat(Arbre::Context.new do
+                      hr
+                      div class: "row" do
+                        div class: "col-12" do
+                          attach.template.render partial: "admin/admin_shared/image_upload", locals: {f: attach}
+                        end
+                      end
+                    end.to_s)
+                  end
                 end
               end
 
               tab 'Meta SEO', style: "list-style-type: none;" do
-                columns do
-                  yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
+                yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
+                panel "Meta SEO" do
+
                   yml_setting[:meta].each do |obj|
-                    column do
-                      f.input obj, input_html: {class: "w-100 form-control"}
+                    case obj
+                    when "meta_description", "keywords", "meta_title"
+                      f.input obj, as: :text, :input_html => { :class => "form-control w-100 mb-3", rows: 5 }
+                    else
+                      f.input obj, input_html: {class: "form-control w-100 mb-3"}
                     end
                   end
                 end
