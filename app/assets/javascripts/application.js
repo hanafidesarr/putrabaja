7b10c962714c57e600a2cd813d9f7a2105c06bba
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-10-25 21:38:01 +0700

    modif banner flip effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require frontend/lib/split_product_showcase/js/modernizr.custom.js
 //= require frontend/lib/split_product_showcase/js/slider.js
 //= require frontend/lib/modern_slide_in/sequence.min.js
-
+//= require frontend/lib/page_flip/pageflip-min.js
 //= require loadjs.min
