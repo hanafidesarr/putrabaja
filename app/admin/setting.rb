da159885d8879fe3a28e45569859529045074b6a
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2022-12-21 22:22:42 +0700

    redesign component slick_slider_multiple

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -5,7 +5,8 @@ ActiveAdmin.register Setting do
 
   yml_setting = HashWithIndifferentAccess.new(YAML.load_file Rails.root.join('config', 'setting.yml'))
   permit_params :name, :phone, :email, :phone_1, :phone_2, :address, :location, :youtube_url, :map_url, :short_description, :description, yml_setting[:url_media_socials], yml_setting[:general_style], yml_setting[:meta],
-  images_attributes: [:id, :asset, :title, :active, :layout_type, :url, :alt, :note, :_destroy] 
+  images_attributes: [:id, :active, :asset, :content_type, :note, :title, :subtitle, :layout_type, :image_quality, :background_image_size, :background_image_position, :background_image_color, :url_image, :url_image_thumb, :price, :series, :width, :height, :url, :alt, :_destroy] 
+  
 
   include SettingAdmin::Index
   include SettingAdmin::Form
