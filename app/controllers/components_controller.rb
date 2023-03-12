dabd9fae695221c970f081bbd7f4893db4764d32
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2023-03-12 09:46:19 +0700

    [RC-0010] [BACKEND] [ADMIN] Fixing drag position
    [RC-0010] [BACKEND] [ADMIN] Fixing drag position 2

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -2,5 +2,11 @@ class ComponentsController < ApplicationController
   def update_position
     @component = Component.find(params[:id])
     @component.set_list_position(params[:position_update].to_i)
+
+    # Page.all.each do |todo_list|
+    #   todo_list.components.order(:updated_at).each.with_index(1) do |todo_item, index|
+    #     todo_item.update_column :position, index
+    #   end
+    # end
   end
 end
