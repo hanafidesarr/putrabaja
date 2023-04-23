8615ac950246f43401da2c34c76d150469b9089e
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2023-04-23 15:11:10 +0700

    [RC-0033][HEADER] Layout navbar animation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_004741) do
+ActiveRecord::Schema.define(version: 2023_04_23_073418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 2023_04_20_004741) do
     t.string "meta_image"
     t.string "display_name"
     t.string "slug"
+    t.string "link"
     t.index ["locale"], name: "index_page_translations_on_locale"
     t.index ["page_id"], name: "index_page_translations_on_page_id"
   end
